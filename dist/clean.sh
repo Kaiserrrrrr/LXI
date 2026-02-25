983b8606a777029ad0695c8580383dcb5bec45a2
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-25 22:32:07 +0800

    Update clean.sh

--- dist/clean.sh
+++ dist/clean.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-sudo find /usr/share/man -type d -name "*_*" -not -name "en*" -exec rm -rf {} +
-sudo find /usr/share/locale -maxdepth 1 -type d -not -name "en*" -not -name "locale.alias" -not -name "locale" -exec rm -rf {} +
+sudo find /usr/share/man -type d -name "*_*" -not -name "en*" -exec rm -rf {} + >/dev/null 2>&1
+sudo find /usr/share/locale -maxdepth 1 -type d -not -name "en*" -not -name "locale.alias" -not -name "locale" -exec rm -rf {} + >/dev/null 2>&1
 
-{ lscpu | grep -iq "Intel" || sudo rm -rf /usr/lib/firmware/intel*; }
-{ lspci | grep -iqE "amd|radeon" || sudo rm -rf /usr/lib/firmware/amdgpu /usr/lib/firmware/radeon; }
-{ lspci | grep -iq "nvidia" || sudo rm -rf /usr/lib/firmware/nvidia; }
-sudo rm -rf /usr/lib/firmware/{liquidio,qcom,netronome,nfp,mellanox,kaweth,ti-connectivity,mediatek,marvell,cavium,qed}
+{ lscpu | grep -iq "Intel" || sudo rm -rf /usr/lib/firmware/intel*; } >/dev/null 2>&1
+{ lspci | grep -iqE "amd|radeon" || sudo rm -rf /usr/lib/firmware/amdgpu /usr/lib/firmware/radeon; } >/dev/null 2>&1
+{ lspci | grep -iq "nvidia" || sudo rm -rf /usr/lib/firmware/nvidia; } >/dev/null 2>&1
+sudo rm -rf /usr/lib/firmware/{liquidio,qcom,netronome,nfp,mellanox,kaweth,ti-connectivity,mediatek,marvell,cavium,qed} >/dev/null 2>&1
 
-sudo journalctl --vacuum-size=5M
+sudo journalctl --vacuum-size=5M >/dev/null 2>&1
 sudo sed -i 's/#SystemMaxUse=/SystemMaxUse=5M/' /etc/systemd/journald.conf
 sudo sed -i 's/#RuntimeMaxUse=/RuntimeMaxUse=5M/' /etc/systemd/journald.conf
-sudo systemctl restart systemd-journald
-sudo find /var/log -type f -exec truncate -s 0 {} +
+sudo systemctl restart systemd-journald >/dev/null 2>&1
+sudo find /var/log -type f -exec truncate -s 0 {} + >/dev/null 2>&1
 
-sudo find /usr/lib/modules -name "*.ko" -print0 | xargs -0 -r -P $(nproc) strip --strip-debug
+sudo find /usr/lib/modules -name "*.ko" -print0 | xargs -0 -r -P $(nproc) strip --strip-debug >/dev/null 2>&1
 
 orphans=$(pacman -Qtdq)
 if [[ -n "$orphans" ]]; then
-    sudo pacman -Rns $orphans --noconfirm
+    sudo pacman -Rns $orphans --noconfirm >/dev/null 2>&1
 fi
 
-sudo pacman -Scc --noconfirm 
-rm -rf ~/.cache/thumbnails/* ~/.cache/fontconfig/* ~/.local/share/Trash/* 
+sudo pacman -Scc --noconfirm >/dev/null 2>&1
+rm -rf ~/.cache/thumbnails/* ~/.cache/fontconfig/* ~/.local/share/Trash/*
 echo -e "\e[32m[✓] Cleanup Complete\e[0m"
