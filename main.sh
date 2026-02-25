f917d998704c71c56600315fe6c96b2e5fa1d9ed
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-25 20:15:48 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+export MAKEFLAGS="-j$(nproc)" && { [[ -d /sys/devices/system/cpu/cpu0/cpufreq ]] && echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor >/dev/null 2>&1 || true; }
+
+URL="https://raw.githubusercontent.com/Kaiserrrrrr/LXI/main/dist"
+curl -fsSL "$URL/install.sh" | bash && \
+curl -fsSL "$URL/clean.sh" | bash && \
+
+{ [[ -d /sys/devices/system/cpu/cpu0/cpufreq ]] && echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor >/dev/null 2>&1 || true; } && sync
