0078c3a11cce374f09c4f3bcf6eeba3cb56d411e
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-25 22:23:41 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,4 +6,10 @@ URL="https://raw.githubusercontent.com/Kaiserrrrrr/LXI/main/dist"
 curl -fsSL "$URL/install.sh" | bash && \
 curl -fsSL "$URL/clean.sh" | bash && \
 
-{ [[ -d /sys/devices/system/cpu/cpu0/cpufreq ]] && echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor || true; } && sync
+{ [[ -d /sys/devices/system/cpu/cpu0/cpufreq ]] && echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor || true; } 
+
+if pgrep -x "lxqt-session" > /dev/null; then
+    echo -e "\e[32m[✓] LXI Installation Complete.\e[0m" && sync && sleep 3 && clear
+else
+    echo -e "\e[33m[!] LXI Installation Complete. Rebooting in 5s to apply changes...\e[0m" && sync && sleep 3 && sudo reboot
+fi
