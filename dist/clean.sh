db0f4bb62650b368339e69b587f59f478017fa8d
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-25 22:07:17 +0800

    Update clean.sh

--- dist/clean.sh
+++ dist/clean.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-sudo find /usr/share/man -type d -name "*_*" -not -name "en*" -exec rm -rf {}
-sudo find /usr/share/locale -type d -not -name "en*" -not -name "locale.alias" -delete 
+sudo find /usr/share/man -type d -name "*_*" -not -name "en*" -exec rm -rf {} +
+sudo find /usr/share/locale -maxdepth 1 -type d -not -name "en*" -not -name "locale.alias" -not -name "locale" -exec rm -rf {} +
 
-{ sudo pacman -Rns adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-cn-fonts ttf-droid --noconfirm || true; }
-{ lscpu 2>/dev/null | grep -iq "Intel" || sudo rm -rf /usr/lib/firmware/intel* }
-{ lspci 2>/dev/null | grep -iqE "amd|radeon" || sudo rm -rf /usr/lib/firmware/amdgpu /usr/lib/firmware/radeon }
-{ lspci 2>/dev/null | grep -iq "nvidia" || sudo rm -rf /usr/lib/firmware/nvidia }
+{ lscpu | grep -iq "Intel" || sudo rm -rf /usr/lib/firmware/intel*; }
+{ lspci | grep -iqE "amd|radeon" || sudo rm -rf /usr/lib/firmware/amdgpu /usr/lib/firmware/radeon; }
+{ lspci | grep -iq "nvidia" || sudo rm -rf /usr/lib/firmware/nvidia; }
 sudo rm -rf /usr/lib/firmware/{liquidio,qcom,netronome,nfp,mellanox,kaweth,ti-connectivity,mediatek,marvell,cavium,qed}
 
+sudo journalctl --vacuum-size=5M
+sudo sed -i 's/#SystemMaxUse=/SystemMaxUse=5M/' /etc/systemd/journald.conf
+sudo sed -i 's/#RuntimeMaxUse=/RuntimeMaxUse=5M/' /etc/systemd/journald.conf
+sudo systemctl restart systemd-journald
+sudo find /var/log -type f -exec truncate -s 0 {} +
+
 { sudo find /usr/lib/modules -name "*.ko" -print0 | xargs -0 -P $(nproc) strip --strip-debug || true; }
-{ orphans=$(pacman -Qtdq) && [[ -n "$orphans" ]] && sudo pacman -Rns $orphans --noconfirm || true; }
+
+orphans=$(pacman -Qtdq)
+if [[ -n "$orphans" ]]; then
+    sudo pacman -Rns $orphans --noconfirm
+fi
+
 sudo pacman -Scc --noconfirm 
-rm -rf ~/.cache/thumbnails/* ~/.cache/fontconfig/* ~/.local/share/Trash/* 
-sudo find /var/log -type f -exec truncate -s 0 {} + && sudo journalctl --vacuum-time=1s
-echo -e "\e[32m[✓] Cleanup Complete\e[0m"
+rm -rf ~/.cache/thumbnails/* ~/.cache/fontconfig/* ~/.local/share/Trash/* echo -e "\e[32m[✓] Cleanup Complete\e[0m"
