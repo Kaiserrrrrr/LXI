4df94daa59f16dfb7d1c21582a50cba1805c8b99
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-25 22:32:48 +0800

    Update install.sh

--- dist/install.sh
+++ dist/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo pacman -Syu --noconfirm --needed 
+sudo pacman -Syu --noconfirm --needed >/dev/null 2>&1
+
 CPU_VENDOR=$(grep -m1 'vendor_id' /proc/cpuinfo)
 IS_LAPTOP=$([[ -d /sys/class/power_supply/BAT0 || -d /sys/class/input/mouse0 ]] && echo 1 || echo 0)
 DRIVERS="sof-firmware alsa-firmware tlp thermald mesa udisks2 zram-generator"
@@ -11,8 +12,10 @@ DRIVERS="sof-firmware alsa-firmware tlp thermald mesa udisks2 zram-generator"
 { [[ "$IS_LAPTOP" == "1" ]] && DRIVERS+=" xf86-input-libinput" || true; }
 { grep -q "^\[multilib\]" /etc/pacman.conf && DRIVERS+=" lib32-mesa" || true; }
 
-sudo pacman -S --noconfirm --needed $DRIVERS openbox lxqt-session lxqt-panel lxqt-config lxqt-qtplugin lxqt-powermanagement lxqt-notificationd lxqt-policykit lxqt-globalkeys lxqt-runner lxqt-themes lximage-qt pcmanfm-qt qterminal breeze-icons gvfs light-locker xorg-server lightdm lightdm-gtk-greeter pipewire-audio pavucontrol-qt alsa-utils bluez bluez-utils blueman networkmanager network-manager-applet xdg-utils picom
-{ [[ ! -f /etc/systemd/zram-generator.conf ]] && echo -e "[zram0]\nzram-size = ram * 0.6\ncompression-algorithm = zstd\nswap-priority = 100\nfs-type = swap" | sudo tee /etc/systemd/zram-generator.conf || true; }
-sudo systemctl daemon-reload && sudo usermod -aG video,audio,lp,scanner $USER 
-sudo systemctl enable lightdm bluetooth NetworkManager tlp thermald
+sudo pacman -S --noconfirm --needed $DRIVERS openbox lxqt-session lxqt-panel lxqt-config lxqt-qtplugin lxqt-powermanagement lxqt-notificationd lxqt-policykit lxqt-globalkeys lxqt-runner lxqt-themes lximage-qt pcmanfm-qt qterminal breeze-icons gvfs light-locker xorg-server lightdm lightdm-gtk-greeter pipewire-audio pavucontrol-qt alsa-utils bluez bluez-utils blueman networkmanager network-manager-applet xdg-utils picom >/dev/null 2>&1
+
+{ [[ ! -f /etc/systemd/zram-generator.conf ]] && echo -e "[zram0]\nzram-size = ram * 0.6\ncompression-algorithm = zstd\nswap-priority = 100\nfs-type = swap" | sudo tee /etc/systemd/zram-generator.conf >/dev/null || true; }
+sudo systemctl daemon-reload >/dev/null 2>&1 && sudo usermod -aG video,audio,lp,scanner $USER >/dev/null 2>&1
+sudo systemctl enable lightdm bluetooth NetworkManager tlp thermald >/dev/null 2>&1
+
 echo -e "\e[32m[✓] Installation Complete\e[0m"
