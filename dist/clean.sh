f8a05da9884b52e29725f2eb5e89ed3f6fff25c6
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-27 19:52:29 +0800

    Update clean.sh

--- dist/clean.sh
+++ dist/clean.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo find /usr/share/man -type d -name "*_*" -not -name "en*" -exec rm -rf {} + >/dev/null 2>&1
-sudo find /usr/share/locale -maxdepth 1 -type d -not -name "en*" -not -name "locale.alias" -not -name "locale" -exec rm -rf {} + >/dev/null 2>&1
+# sudo find /usr/share/man -type d -name "*_*" -not -name "en*" -exec rm -rf {} + >/dev/null 2>&1
+# sudo find /usr/share/locale -maxdepth 1 -type d -not -name "en*" -not -name "locale.alias" -not -name "locale" -exec rm -rf {} + >/dev/null 2>&1
 
-{ lscpu | grep -iq "Intel" || sudo rm -rf /usr/lib/firmware/intel*; } >/dev/null 2>&1
-{ lspci | grep -iqE "amd|radeon" || sudo rm -rf /usr/lib/firmware/amdgpu /usr/lib/firmware/radeon; } >/dev/null 2>&1
-{ lspci | grep -iq "nvidia" || sudo rm -rf /usr/lib/firmware/nvidia; } >/dev/null 2>&1
-sudo rm -rf /usr/lib/firmware/{liquidio,qcom,netronome,nfp,mellanox,kaweth,ti-connectivity,mediatek,marvell,cavium,qed} >/dev/null 2>&1
+# { lscpu | grep -iq "Intel" || sudo rm -rf /usr/lib/firmware/intel*; } >/dev/null 2>&1
+# { lspci | grep -iqE "amd|radeon" || sudo rm -rf /usr/lib/firmware/amdgpu /usr/lib/firmware/radeon; } >/dev/null 2>&1
+# { lspci | grep -iq "nvidia" || sudo rm -rf /usr/lib/firmware/nvidia; } >/dev/null 2>&1
+# sudo rm -rf /usr/lib/firmware/{liquidio,qcom,netronome,nfp,mellanox,kaweth,ti-connectivity,mediatek,marvell,cavium,qed} >/dev/null 2>&1
 
 sudo journalctl --vacuum-size=5M >/dev/null 2>&1
 sudo sed -i 's/#SystemMaxUse=/SystemMaxUse=5M/' /etc/systemd/journald.conf
