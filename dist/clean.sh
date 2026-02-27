2f30450ebcf7fe655022e24f659ebbe7d0ad56ef
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-27 22:13:03 +0800

    Update clean.sh

--- dist/clean.sh
+++ dist/clean.sh
@@ -11,6 +11,23 @@ if [[ -n "$orphans" ]]; then
     sudo pacman -Rns $orphans --noconfirm >/dev/null 2>&1
 fi
 
+hw_data=$(lspci -k; lsusb)
+
+if [[ -n "$hw_data" ]]; then
+        for pkg in $(pacman -Qqs linux-firmware-); do
+        
+        vendor=${pkg#linux-firmware-}
+        
+        if [[ "$vendor" == "whence" || "$vendor" == "other" ]]; then
+            continue
+        fi
+        
+        if ! echo "$hw_data" | grep -wiq "$vendor"; then
+            sudo pacman -Rns --noconfirm "$pkg" >/dev/null 2>&1
+        fi
+    done
+fi
+
 sudo pacman -Scc --noconfirm >/dev/null 2>&1
 rm -rf ~/.cache/thumbnails/* ~/.cache/fontconfig/* ~/.local/share/Trash/*
 echo -e "\e[32m[✓] Cleanup Complete\e[0m"
