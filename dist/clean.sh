e90194275be5eb7e193f47bf1ecc645cf70ab89b
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-27 19:20:25 +0800

    Update clean.sh

--- dist/clean.sh
+++ dist/clean.sh
@@ -14,8 +14,6 @@ sudo sed -i 's/#RuntimeMaxUse=/RuntimeMaxUse=5M/' /etc/systemd/journald.conf
 sudo systemctl restart systemd-journald >/dev/null 2>&1
 sudo find /var/log -type f -exec truncate -s 0 {} + >/dev/null 2>&1
 
-sudo find /usr/lib/modules -name "*.ko" -print0 | xargs -0 -r -P $(nproc) strip --strip-debug >/dev/null 2>&1
-
 orphans=$(pacman -Qtdq)
 if [[ -n "$orphans" ]]; then
     sudo pacman -Rns $orphans --noconfirm >/dev/null 2>&1
