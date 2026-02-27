b2d12698538762fe0a5f6ba861185a1b60f14699
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-27 23:29:42 +0800

    Update install.sh

--- dist/install.sh
+++ dist/install.sh
@@ -17,6 +17,6 @@ pacman -S --noconfirm --needed $DRIVERS lxqt-session lxqt-panel lxqt-runner lxqt
 { [[ ! -f /etc/systemd/zram-generator.conf ]] && echo -e "[zram0]\nzram-size = ram * 0.6\ncompression-algorithm = zstd\nswap-priority = 100\nfs-type = swap" | sudo tee /etc/systemd/zram-generator.conf >/dev/null || true; }
 systemctl daemon-reload >/dev/null 2>&1 && sudo usermod -aG video,audio,lp,scanner $USER >/dev/null 2>&1
 systemctl enable lightdm bluetooth NetworkManager tlp thermald >/dev/null 2>&1
-sudo -u "$SUDO_USER:-$(logname)}" systemctl --user enable --now pipewire pipewire-pulse wireplumber
+sudo -u "$SUDO_USER:-$(logname)}" systemctl --user enable --now pipewire pipewire-pulse wireplumber >/dev/null 2>&1
 
 echo -e "\e[32m[✓] Installation Complete\e[0m"
