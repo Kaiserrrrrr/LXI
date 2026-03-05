bfce4b549f17e54755aba03b60a28010e4b178c4
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-05 16:14:52 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 URL="https://raw.githubusercontent.com/Kaiserrrrrr/LXI/main/dist"
-curl -fsSL "$URL/install.sh" | bash && \
-curl -fsSL "$URL/clean.sh" | bash && \
 
 if pgrep -x "lxqt-session" > /dev/null; then
+    curl -fsSL "$URL/install.sh" | sh && \
+    curl -fsSL "$URL/clean.sh" | sh && \
     echo -e "\e[32m[✓] LXI Update Complete.\e[0m" && sync && sleep 3 && clear
 else
+    curl -fsSL "$URL/install.sh" | sh && \
+    curl -fsSL "$URL/config.sh" | sh && \
+    curl -fsSL "$URL/clean.sh" | sh && \
     echo -e "\e[33m[!] LXI Installation Complete. Rebooting in 5s to apply changes...\e[0m" && sync && sleep 3 && sudo reboot
 fi
