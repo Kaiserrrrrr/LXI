2fc370de0c09aab2f16e1162830cb58134ae3c64
Kaiser <97164185+kaiserrrrrr@users.noreply.github.com>
2026-03-16 16:40:47 +0800

    Update main.sh

--- src/de/main.sh
+++ src/de/main.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+sudo -v; clear
+
+URL="https://raw.githubusercontent.com/kaiserrrrrr/aqtive/main/src/de"
+
+PKG=$(curl -s $URL/$DE/install.txt)
+sudo pacman -Syu --noconfirm && sudo pacman $PKG --noconfirm && sudo pacman -Scc --noconfirm
