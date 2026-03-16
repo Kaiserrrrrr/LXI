be3bdc0628ea56ee4dfc57e3b3dff775b94ce0d7
Kaiser <97164185+kaiserrrrrr@users.noreply.github.com>
2026-03-16 17:21:22 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -19,16 +19,18 @@ sudo -v; clear && echo '
 
 ' | cat
 
-URL="https://raw.githubusercontent.com/kaiserrrrrr/aqtive/main/src/applications"
+URL="https://raw.githubusercontent.com/kaiserrrrrr/aqtive/main/src/"
+
 reboot_now() {  read -p "Reboot now to apply changes? [Y/n] " res < /dev/tty; case "$res" in [Yy]*) reboot ;; [Nn]*) clear && return 0 ;; *) clear && return 0 ;; esac; }
+de_install() {  read -p "Install Desktop Environment? [Y/n] " res < /dev/tty; case "$res" in [Yy]*) curl -fsSL "$URL/de/main.sh" | sh ;; [Nn]*) reboot_now ;; *) clear && return 0 ;; esac; }
 
 if [[ "$XDG_SESSION_TYPE" == "x11" || "$XDG_SESSION_TYPE" == "wayland" || -n "$DISPLAY" || -n "$WAYLAND_DISPLAY" ]]; then
-    curl -fsSL "$URL/install.sh" | sh && \
-    curl -fsSL "$URL/clean.sh" | sh && \
+    curl -fsSL "$URL/applications/install.sh" | sh && \
+    curl -fsSL "$URL/applications/clean.sh" | sh && \
     sync && echo -n "Update Complete. " && reboot_now
 else
-    curl -fsSL "$URL/install.sh" | sh && \
-    curl -fsSL "$URL/config.sh" | sh && \
-    curl -fsSL "$URL/clean.sh" | sh && \
-    sync && echo -n "Installation Complete. " && reboot_now
+    curl -fsSL "$URL/applications/install.sh" | sh && \
+    curl -fsSL "$URL/applications/config.sh" | sh && \
+    curl -fsSL "$URL/applications/clean.sh" | sh && \
+    sync && echo -n "Installation Complete. " && de_install
 fi
